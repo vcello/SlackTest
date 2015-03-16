e1a4a2c39e44c301ee28a8ab6a843e781d36f8e2
vcello <matthew.john.twomey@gmail.com>
2015-03-15 22:54:20 -0700

    typos kill

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 <script>
 // click handler
 $(document).ready(function(){
-	$("#tag").find("span").click(
+	$("#tags").find("span").click(
 		function() {
 			$("span").css("background", "white"); // unhighlight previously clicked tag
 			$("#htmlContent").css("background", "white"); // unhighlight html
